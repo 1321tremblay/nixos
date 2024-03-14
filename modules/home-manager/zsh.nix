fb7e43c7932fd4203b35bb29a5021349e2982bde
1321tremblay <oliviertremblay13@gmail.com>
2024-03-13 22:05:57 -0400

    Added clean boot alias to zsh

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -10,8 +10,9 @@
 
   shellAliases = {
     ll = "ls -l";
-    update = "sudo nixos-rebuild switch";
+    update = "sudo nixos-rebuild switch --flake ~/nixos/#main";
     nvx = "nix run ~/nixos/modules/home-manager/nixvim";
+    ncb = " sudo nix-collect-garbage --delete-old && sudo /run/current-system/bin/switch-to-configuration boot";
   };
   history.size = 10000;
   history.path = "${config.xdg.dataHome}/zsh/history";
